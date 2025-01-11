42b35f4aaf66cfb32ab1ec561a6aa39c1f99f21b
nima <niimak97@gmail.com>
2025-01-11 18:39:37 +0330

    using ServiceLoader to implement loose coupling

--- src/main/java/com/illuutech/Main.java
+++ src/main/java/com/illuutech/Main.java
@@ -1,13 +1,21 @@
 package com.illuutech;
 
+import java.util.ServiceLoader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private static final String GREETING = "Hello ";
 
     public static void main(String[] args) {
-        MessageProvider mp = new MessageProviderImpl();
-        MessageRenderer mr = new MessageRendererImpl();
+        ServiceLoader<MessageRenderer> messageRendererServiceLoader = ServiceLoader.load(MessageRenderer.class);
+        ServiceLoader<MessageProvider> messageProviderServiceLoader = ServiceLoader.load(MessageProvider.class);
+        MessageRenderer mr = messageRendererServiceLoader.findFirst()
+                .orElseThrow(()->new RuntimeException("Couldn't find a valid Implementation of class: "
+                        + MessageRenderer.class.getName()));
+        MessageProvider mp = messageProviderServiceLoader.findFirst()
+                .orElseThrow(()->new RuntimeException("Couldn't find a valid Implementation of class: "
+                        + MessageProvider.class.getName()));
         mr.setMessageProvider(mp);
         mr.render();
     }
